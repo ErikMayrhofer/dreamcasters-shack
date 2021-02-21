7684693043133305b38048f350e33edb64d7123f
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-02-21 19:32:50 +0100

    added footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,8 +1,56 @@
-<footer>Footer :)</footer>
+<footer>
+  <section>
+    <h1>Team</h1>
+    <ul>
+      <li>Celina Niederhuber</li>
+      <li>Johanna Einramhof</li>
+    </ul>
+    <h2>Technical Support</h2>
+    <ul>
+      <li>Erik Mayrhofer</li>
+    </ul>
+  </section>
+  <section>
+    <h1>Follow Us</h1>
+    <ul>
+      <li>Etsy</li>
+      <li>Tumblr</li>
+      <li>Facebook</li>
+      <li>Instagram</li>
+    </ul>
+  </section>
+  <section>
+    <h1>Impressum</h1>
+    <ul>
+      <li>Tel</li>
+      <li>Email</li>
+      <li>Firmenbuchnr odaso</li>
+      <li>etc</li>
+    </ul>
+  </section>
+</footer>
 
 <style>
   footer {
     position: relative;
     padding: 2em 0;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+  }
+  h1,
+  h2 {
+    opacity: 0.8;
+    padding: 0;
+    text-align: center;
   }
 </style>
